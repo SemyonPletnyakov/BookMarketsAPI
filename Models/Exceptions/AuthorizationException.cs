bfa66917c6175e1765d34d17c718c7c66b85f052
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2024-04-30 19:37:52 +0300

    Добавление исключений для ошибки авторизации и недостаточности прав

---
+++ Models/Exceptions/AuthorizationException.cs
@@ -0,0 +1,40 @@
+﻿namespace Models.Exceptions;
+
+/// <summary>
+/// Исключение, выбрасываемое при ошибке авторизации.
+/// </summary>
+public class AuthorizationException : Exception
+{
+    /// <summary>
+    /// Создаёт объект <see cref="AuthorizationException"/>.
+    /// </summary>
+    public AuthorizationException()
+        : base()
+    {
+    }
+
+    /// <summary>
+    /// Создаёт объект <see cref="AuthorizationException"/>.
+    /// </summary>
+    /// <param name="message">
+    /// Сообщение исключения.
+    /// </param>
+    public AuthorizationException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Создаёт объект <see cref="AuthorizationException"/>.
+    /// </summary>
+    /// <param name="message">
+    /// Сообщение исключения.
+    /// </param>
+    /// <param name="inner">
+    /// Внутреннее исключение.
+    /// </param>
+    public AuthorizationException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
---
+++ Models/Exceptions/NotEnoughRightsException.cs
@@ -0,0 +1,40 @@
+﻿namespace Models.Exceptions;
+
+/// <summary>
+/// Исключение, выбрасываемое при недостаточных правах для операции.
+/// </summary>
+public class NotEnoughRightsException : Exception
+{
+    /// <summary>
+    /// Создаёт объект <see cref="NotEnoughRightsException"/>.
+    /// </summary>
+    public NotEnoughRightsException()
+        : base()
+    {
+    }
+
+    /// <summary>
+    /// Создаёт объект <see cref="NotEnoughRightsException"/>.
+    /// </summary>
+    /// <param name="message">
+    /// Сообщение исключения.
+    /// </param>
+    public NotEnoughRightsException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Создаёт объект <see cref="NotEnoughRightsException"/>.
+    /// </summary>
+    /// <param name="message">
+    /// Сообщение исключения.
+    /// </param>
+    /// <param name="inner">
+    /// Внутреннее исключение.
+    /// </param>
+    public NotEnoughRightsException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
