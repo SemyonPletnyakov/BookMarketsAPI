5d477e0c25d5225e5338de66512279fe08292804
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2024-05-02 22:24:47 +0300

    Добавлен хелпер для вытаскивания jwt Токена из хендлеров. Удалён сгенерированный класс погоды. Добавлены ссылки на другие проекты в BookMarketsAPI.

---
+++ BookMarketsAPI/Helpers/AuthorizationHelper.cs
@@ -0,0 +1,15 @@
+﻿using Models.Autorization;
+
+namespace BookMarketsAPI.Helpers;
+
+public static class AuthorizationHelper
+{
+    public static JwtToken GetJwtTokenFromHandlers(this IHeaderDictionary headers)
+    {
+        var token = (headers.ContainsKey("authorization")
+            ? headers["authorization"]
+            : headers["Authorization"]).ToString().Replace("Bearer ", "");
+
+        return new(token);
+    }
+}
