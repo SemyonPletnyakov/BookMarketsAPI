8a905a3c730e92bce6cc67d8815e5aeffa491439
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2024-05-03 23:20:29 +0300

    Добавление процессора без атворизации, возращающего только задачу.

---
+++ Logic/Abstractions/Processors/IRequestProcessorWithoutAuthorize{TInput}.cs
@@ -0,0 +1,26 @@
+﻿using Models.Requests.BaseRequests;
+
+namespace Logic.Abstractions.Processors;
+
+/// <summary>
+/// Процессор запросов без авторизации.
+/// </summary>
+/// <typeparam name="TInput">
+/// Запрос.
+/// </typeparam>
+public interface IRequestProcessorWithoutAuthorize<TInput> 
+    where TInput : RequestBase
+{
+    /// <summary>
+    /// Обрабатывает запрос.
+    /// </summary>
+    /// <param name="request">
+    /// Запрос.
+    /// </param>
+    /// <param name="token">
+    /// Токен отмены.
+    /// </param>
+    public Task ProcessAsync(
+        TInput request,
+        CancellationToken token);
+}
---
+++ Logic/Processors/RequestProcessorWithoutAuthorize{TInput}.cs
@@ -0,0 +1,49 @@
+﻿using Logic.Abstractions.Handlers;
+using Logic.Abstractions.Processors;
+
+using Models.Requests.BaseRequests;
+
+namespace Logic.Processors;
+
+/// <summary>
+/// Процессор запросов с авторизацией.
+/// </summary>
+/// <typeparam name="TInput">
+/// Запрос.
+/// </typeparam>
+public sealed class RequestProcessorWithoutAuthorize<TInput> :
+    IRequestProcessorWithoutAuthorize<TInput>
+    where TInput : RequestBase
+{
+    /// <summary>
+    /// Создаёт объект <see cref="RequestProcessorWithoutAuthorize{TInput}"/>.
+    /// </summary>
+    /// <param name="ruleChecker">
+    /// Сущность, проверяющая, 
+    /// достаточно ли прав у пользователя на совершение операции.
+    /// </param>
+    /// <param name="requestHandler">
+    /// Обработчик запроса.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Если <paramref name="requestHandler"/> равен <see langword="null"/>.
+    /// </exception>
+    public RequestProcessorWithoutAuthorize(IRequestHandler<TInput> requestHandler)
+    {
+        _requestHandler = requestHandler 
+            ?? throw new ArgumentNullException(nameof(requestHandler));
+    }
+
+    /// <inheritdoc/>
+    public Task ProcessAsync(
+        TInput request,
+        CancellationToken token)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        token.ThrowIfCancellationRequested();
+
+        return _requestHandler.HandleAsync(request, token);
+    }
+
+    private readonly IRequestHandler<TInput> _requestHandler;
+}
