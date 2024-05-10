82d1ec699ca6f20a146a36d01ff27dceda52b31c
Семён Семёнович Плетняков <pletnyakov01@mail.ru>
2024-05-10 16:50:15 +0300

    Исправление транспортных моделей покупателя

--- Transport/Models/ForCreate/Customer.cs
+++ Transport/Models/ForCreate/Customer.cs
@@ -8,12 +8,12 @@ public sealed class Customer
     /// <summary>
     /// Фамилия.
     /// </summary>
-    public required string? LastName { get; set; }
+    public string? LastName { get; set; }
 
     /// <summary>
     /// Имя.
     /// </summary>
-    public required string? FirstName { get; set; }
+    public string? FirstName { get; set; }
 
     /// <summary>
     /// Отчество.
